95ac2cf9d4a7f51facac44a637241bccaa7167d3
A <14969416+alexmaximo06@users.noreply.github.com>
2019-09-19 22:35:46 -0300

    REST p2: soporte para /medidas y mejora de clases Resources

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, session, redirect, url_for, escape, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-import wtforms
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, IntegerField
-from wtforms.validators import DataRequired
-import flask_wtf, flask_wtf.file
 import os
-
 from time import sleep
-#from functools import wraps # TODO Implementar decoradores de auth
+
+import flask_wtf
+import flask_wtf.file
+import wtforms
+
+from flask import (Flask, escape, jsonify, redirect, render_template, request, session, url_for)
+from flask_migrate import Migrate
+from flask_restful import Api, Resource, abort, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from werkzeug.datastructures import FileStorage
+from wtforms import IntegerField, PasswordField, StringField
+from wtforms.validators import DataRequired
 
 ################################################
 # Flask
@@ -30,8 +32,8 @@ if os.path.isfile('SECRET_KEY'):
 		secret_key = bytes(secret_key, encoding='utf-8')
 
 app.config.update(
-    #TESTING=True,
-    SECRET_KEY=secret_key,
+	#TESTING=True,
+	SECRET_KEY=secret_key,
 	SQLALCHEMY_DATABASE_URI = DB_URI,
 	SQLALCHEMY_TRACK_MODIFICATIONS = False,
 	TEMPLATES_AUTO_RELOAD = True
@@ -40,44 +42,166 @@ app.config.update(
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from flask_restful import Resource, Api, reqparse, abort
-api = Api(app)
+api = Api(app, catch_all_404s=True)
 
 # Logger
-LOG = 1
-DEFAULT_LOG = "Default"
-LOGIN_LOG = "Login"
+LOG_ENABLED = 1
 
-def logger(logtype=DEFAULT_LOG, logmsg=""):
-	if LOG: print(">>> [%s] %s" % (logtype, logmsg))
 
-parser = reqparse.RequestParser()
-parser.add_argument('email', required=True, help="email")
-parser.add_argument('pwd', required=True, help="pwd")
+# Nuevo resource con GET por defecto y sistema de logging
+class NewResource(Resource):
+	LOG_TAG = "Default"
 
-class v2Login(Resource):
+	def log(self, msg=""):
+		if LOG_ENABLED: print(">>> [%s] %s" % (self.LOG_TAG, msg))
+
 	def get(self):
-		return {'hello': 'world'}
+		return {'message': 'GET %s' % self.__class__.__name__}
+	
+
+class v2Login(NewResource):
+	LOG_TAG = "Login"
+
 	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('email', help="email missing", required=True)
+		parser.add_argument('pwd', help="pwd missing", required=True)
 		args = parser.parse_args()
 		email = args['email']
 		pwd = args['pwd']
 
-		logger(LOGIN_LOG, "%s:%s" % (email, pwd))
+		self.log("%s:%s" % (email, pwd))
 
 		u = Usuario.getUsuarioByEmail(email)
 		if u is None:
-			logger(LOGIN_LOG, "Email no existe")
+			self.log("Email no existe")
 			return abort(401, message="Bad login")
 		if not u.validPassword(pwd):
-			logger(LOGIN_LOG, "Password incorrecta")
+			self.log("Password incorrecta")
 			return abort(401, message="Bad login")
-		logger(LOGIN_LOG, "%s autenticado" % u)		
-		return {"response": "logueado", "token": str(u.id)}
+		self.log("%s autenticado" % u)		
+		return {"status": "ok", "token": str(u.id)}
 
 
-api.add_resource(v2Login, '/v2/login')
+class v2Medidas(NewResource):
+	LOG_TAG = "Medidas"
 
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('token', help="token missing", required=True, location='headers')
+		args = parser.parse_args()
+		token = args['token']
+
+		self.log("Token %s" % (token))
+
+		u = decodeToken(token)
+		if u is None:
+			self.log("Usuario no encontrado")
+			return abort(401, message="Bad token")
+		self.log("%s autorizado" % u)
+		return {"medidas": {
+					'right_arm': u.right_arm,
+					'left_arm': u.left_arm,
+					'right_leg': u.right_leg,
+					'left_leg': u.left_leg,
+					'waist': u.waist,
+					'hip': u.hip,
+					'chest': u.chest,
+					'bust': u.bust,}
+		}
+
+
+class v2Perfil(NewResource):
+	LOG_TAG = "Perfil"
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('token', help="token missing", required=True, location='headers')
+		args = parser.parse_args()
+		token = args['token']
+
+		self.log("Token %s" % (token))
+
+		u = decodeToken(token)
+		if u is None:
+			self.log("Usuario no encontrado")
+			return abort(401, message="Bad token")
+		self.log("%s autorizado" % u)
+		return {"perfil": {
+					'email': u.email,
+					'nombre': u.nombre,
+					'rut': u.rut,}
+		}
+
+
+class v2Upload(NewResource):
+	"""Docstring"""
+	LOG_TAG = "Upload"
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('token', help="token missing", required=False, location='headers')
+		from werkzeug.datastructures import FileStorage
+		parser.add_argument("photo", help="photo missing", type=FileStorage, required=True, location='files')
+		parser.add_argument("height", help="height missing", required=True)
+		args = parser.parse_args()
+		token = args['token']
+		photo = args['photo']
+		height = args['height']
+
+		self.log("Photo: %s, height: %s, token: %s" % (photo.filename, height, token))
+		
+		result = tf.handle_photo(photo, height)
+		
+		if result is not None and token is not None:
+			u = decodeToken(token)
+			if u is not None:
+				u.guardarMedidas(result)
+				#self.log("Usuario no encontrado")
+		
+		return {"response": "upload", "result": result}
+
+
+class v2Register(NewResource):
+	LOG_TAG = "Register"
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('email', help="email missing", required=True)
+		parser.add_argument('pwd', help="pwd missing", required=True)
+		parser.add_argument('nombre')
+		parser.add_argument('rut')
+		args = parser.parse_args()
+		email = args['email']
+		pwd = args['pwd']
+		nombre = args['nombre']
+		rut = args['rut']
+
+		self.log("%s:%s:%s:%s" % (email, pwd, nombre, rut))
+
+		u = Usuario.getUsuarioByEmail(email)
+		if u is not None:
+			self.log("Email ya existe")
+			return abort(400, message="Email already exists")
+		
+		u = Usuario.addUsuario(email=email, pwd=pwd, nombre=nombre, rut=rut)
+		token = makeToken(u)
+		self.log("%s registrado" % u)		
+		return {"response": "registrado", "token": token}
+
+
+api.add_resource(v2Login, '/login')
+api.add_resource(v2Perfil, '/perfil')
+api.add_resource(v2Medidas, '/medidas')
+api.add_resource(v2Upload, '/upload')
+api.add_resource(v2Register, '/register')
+
+######################
+# Base de datos
+# Uso de SQLite y SQLAlchemy
+######################
+from model import Usuario, initModel
+from tf import tfManager
 
 ################################################
 # TensorFlow
@@ -86,234 +210,20 @@ api.add_resource(v2Login, '/v2/login')
 # TODO Requests queue, since TF person_detector can't handle simultaneous requests
 ################################################
 TF_ENABLED = 0
-from tf import tfManager
 tf = tfManager(TF_ENABLED)
 #from scripts.testpersona2 import a, b
 #x = person_detector(photo)
 #xx, yy = a()
 #human = 'samples\\single2.jpg'
 
+#from functools import wraps # TODO Implementar decoradores de auth
 
-######################
-# Base de datos
-# Uso de SQLite y SQLAlchemy
-######################
-from model import initModel, Usuario
 #initModel() # Si no existe el .sqlite, usar esta funcion para crear la BD segun los modelos definidos
-
-
-################################################
-# Rutas de Flask
-################################################
-WEB_MODE = 1 # Activar si vamos a hacer GETs y POSTs desde la web; desactivar si se deben devolver solo JSONs
-IGNORE_FORM_VALIDATE = 1 # Activar para ignorar la validacion de las formas recibidas en POST
-
-LOG_REQUESTS = 1
-FAKE_TIMEOUT = 0
-
-# Funcion para responder JSONs
-def responder(s='null', d=None):
-	sleep(FAKE_TIMEOUT)
-
-	RESPUESTA = 'response'
-	RESULTADO_BASE = 'null'
-	RESPUESTA_BASE = {RESPUESTA: RESULTADO_BASE} # {'response': 'null'}
-
-	resp = RESPUESTA_BASE
-
-	# Si d es un diccionario, se usa como base
-	if isinstance(d, dict):
-		resp = d
-
-	# Si s es un string, se usa como respuesta
-	if isinstance(s, str):
-		resp[RESPUESTA] = s # {'response': s}
-			
-	# Si el resultado final de resp es un diccionario, se retorna
-	if isinstance(d, dict):
-		j = jsonify(resp)
-	else:
-		j = jsonify(RESPUESTA_BASE)
-	
-	#if DBG: print(j.response)
-	return j
-
-
-@app.route('/')
-def home():
-	query = Usuario.query.all()
-	return render_template('home.html', q=query) if WEB_MODE else 'Usize_GET_home'
-
-
-class LoginForm(FlaskForm):
-	defaultemail = None
-	defaultpwd = None
-
-	#if DBG:
-		#defaultemail = 'example@email.com'
-		#defaultpwd = 'holahola'
-
-	email = StringField('email', validators=[DataRequired()])
-	pwd = PasswordField('pwd', validators=[DataRequired()])
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	form = LoginForm()
-
-	if request.method == 'POST':
-		if IGNORE_FORM_VALIDATE or form.validate_on_submit():
-			email = form.email.data
-			pwd = form.pwd.data
-
-			if LOG_REQUESTS: print('>>> Login: %s %s' % (email, pwd))
-
-			# Buscamos al usuario en la BD con el email
-			u = Usuario.getUsuarioByEmail(email)
-			# Si existe
-			if u is not None:
-				# Validamos la password con su hash
-				if u.validPassword(pwd):
-					if DBG: print('>>> /login: user_id %d logueado' % u.id)
-					resul = 'logueado'
-					token = makeToken(u)
-					# Caso especial, retornamos de antemano con el token
-					return responder(resul, {'token': token})
-					
-						#return redirect(url_for('home'))
-				else:
-					resul = 'pwd incorrecta'					
-			else:
-				resul = 'email no existe'
-		else:
-			if LOG_REQUESTS: print('>>> Login: Errors: ', form.errors)
-			resul = 'bad_POST_request'
-		
-		return responder(resul)
-
-	return render_template('login.html', form=form) if WEB_MODE else 'Usize_login'
-
-
-class RegisterForm(FlaskForm):
-	email = StringField('email', validators=[DataRequired()])
-	pwd = PasswordField('pwd', validators=[DataRequired()])
-	nombre = StringField('nombre', validators=[])
-	rut = wtforms.IntegerField('rut', validators=[])
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-	form = RegisterForm()
-	errors = None
-
-	if request.method == 'POST':
-		if IGNORE_FORM_VALIDATE or form.validate_on_submit():
-			email = form.email.data
-			pwd = form.pwd.data
-
-			nombre = form.nombre.data
-			rut = form.rut.data
-			
-			if LOG_REQUESTS: print('>>> Register: %s %s %s %d' % (email, pwd, nombre, rut))
-
-			# Buscamos un posible usuario en la BD con el email
-			u = Usuario.getUsuarioByEmail(email)
-			# Si existe, entonces dicho email ya esta registrado
-			if u is not None:
-				errors = 'email ya existe'
-			# Else, registramos al usuario
-			else:
-				resul = 'registrado'
-				u = Usuario.addUsuario(email=email, pwd=pwd, nombre=nombre, rut=rut)
-				token = makeToken(u)
-
-				if WEB_MODE:
-					return redirect(url_for('home'))
-				else:
-					return responder(resul, {'token': token})
-
-				
-	return render_template('register.html', form=form, errors=errors) if WEB_MODE else 'Usize_register_GET'
-
-
-@app.route('/logout', methods=['GET'])
-def logout():
-	session.clear()
-	return redirect(url_for('home'))
-
-
-@app.route('/profile', methods=['GET', 'POST'])
-def profile():
-	if request.method == 'POST':
-		token = request.form['token']
-
-		if LOG_REQUESTS: print('>>> Profile POST: Token %s recibido' % token)
-
-		u = decodeToken(token)
-		if u is not None:
-			if LOG_REQUESTS: print('>>> Profile POST: Usuario %d OK' % u.id)
-
-			return responder('ok', {
-				'email': u.email,
-				'right_arm': u.right_arm,
-				'left_arm': u.left_arm,
-				'right_leg': u.right_leg,
-				'left_leg': u.left_leg,
-				'waist': u.waist,
-				'hip': u.hip,
-				'chest': u.chest,
-				'bust': u.bust,
-				'nombre': u.nombre,
-				'rut': u.rut,
-			})
-
-		if LOG_REQUESTS: print('>>> Profile POST: Usuario no encontrado')
-		return responder('user no encontrado')
-	
-	return 'Usize_profile_GET'
-
-
-class UploadForm(FlaskForm):
-	photo = flask_wtf.file.FileField('photo', validators=[flask_wtf.file.FileRequired()])
-	height = wtforms.IntegerField('height', validators=[DataRequired()])
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-	form = UploadForm()
-
-	if request.method == 'POST':
-		if IGNORE_FORM_VALIDATE or form.validate_on_submit():
-			photo = form.photo.data
-			height = form.height.data
-			#photo = request.files['photo']
-			#photo_strea = photo.read()
-
-			result = tf.handle_photo(photo, height)
-			
-			#if DBG: print(result)
-
-			if isinstance(result, dict):
-				# Si la sesion tiene un usuario, actualizamos las medidas
-				if 'token' in request.form.keys():
-					token = request.form['token']
-					u = decodeToken(token)
-					if u is not None: u.guardarMedidas(result)
-
-				return jsonify(result)
-			else:
-				return jsonify({'result': 'fatal_error'})
-		else:
-			s = 'bad_POST_request'
-			if DBG: print(s)
-			return jsonify({'result': s})
-	
-	return render_template('upload.html', form=form) if WEB_MODE else 'Usize_GET_upload'
-
 
 # Tokens extremadamente basicos: solo la id del usuario
 def makeToken(user):
 	return str(user.id)
+
 
 def decodeToken(token=None):
 	if token is None: return None
@@ -324,3 +234,10 @@ def decodeToken(token=None):
 	except:
 		return None
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return {"message": "404"}
+
+
+#EOF
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-# Instanciamos app.py y activamos la variable app en este archivo
 from app import app
 
+######################
 # main()
-# Iniciamos el servidor Flask
-
+# Iniciamos Flask
+######################
 DEBUG = 1
 PORT = 3333
 
 if __name__ == '__main__':
-	#app.run(host='0.0.0.0', debug=False, port=3333)
-	app.run(host='0.0.0.0', port=5000, debug=False, threaded=False)
+	app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
+

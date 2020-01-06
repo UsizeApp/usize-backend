f7a4f0839c76a9f77d4d1e0692347416022b3bd5
A <14969416+alexmaximo06@users.noreply.github.com>
2020-01-06 17:16:12 -0300

    Cambios retroactivos del Sprint 2

--- app.py
+++ app.py
@@ -28,16 +28,16 @@ DB_URI = 'sqlite:///%s' % DB_FILE
 secret_key = b'_5#y2L"Fqqqqqa14Q8z\n\xec]/'
 
 if os.path.isfile('SECRET_KEY'):
-	with open('SECRET_KEY') as f:
-		secret_key = f.read().strip()
-		secret_key = bytes(secret_key, encoding='utf-8')
+    with open('SECRET_KEY') as f:
+        secret_key = f.read().strip()
+        secret_key = bytes(secret_key, encoding='utf-8')
 
 app.config.update(
-	#TESTING=True,
-	SECRET_KEY=secret_key,
-	SQLALCHEMY_DATABASE_URI = DB_URI,
-	SQLALCHEMY_TRACK_MODIFICATIONS = False,
-	TEMPLATES_AUTO_RELOAD = True
+    #TESTING=True,
+    SECRET_KEY=secret_key,
+    SQLALCHEMY_DATABASE_URI = DB_URI,
+    SQLALCHEMY_TRACK_MODIFICATIONS = False,
+    TEMPLATES_AUTO_RELOAD = True
 )
 
 db = SQLAlchemy(app)
@@ -51,166 +51,180 @@ LOG_ENABLED = 1
 
 # Nuevo resource con GET por defecto y sistema de logging
 class NewResource(Resource):
-	LOG_TAG = "Default"
+    LOG_TAG = "Default"
 
-	def log(self, msg=""):
-		if LOG_ENABLED: print(">>> [%s] %s" % (self.LOG_TAG, msg))
+    def log(self, msg=""):
+        if LOG_ENABLED: print(">>> [%s] %s" % (self.LOG_TAG, msg))
 
-	def get(self):
-		return {'message': 'GET %s' % self.__class__.__name__}
-	
+    def get(self):
+        return {'message': 'GET %s' % self.__class__.__name__}
+        
+    def post(self):
+        self.log('POST %s' % self.__class__.__name__)
+    
 
 class v2Login(NewResource):
-	LOG_TAG = "Login"
+    LOG_TAG = "Login"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('email', help="email missing", required=True)
-		parser.add_argument('pwd', help="pwd missing", required=True)
-		args = parser.parse_args()
-		email = args['email']
-		pwd = args['pwd']
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', help="email missing", required=True)
+        parser.add_argument('pwd', help="pwd missing", required=True)
+        args = parser.parse_args()
+        email = args['email']
+        pwd = args['pwd']
 
-		self.log("%s:%s" % (email, pwd))
+        self.log("%s:%s" % (email, pwd))
 
-		u = Usuario.getUsuarioByEmail(email)
-		if u is None:
-			self.log("Email no existe")
-			return abort(401, message="Bad login")
-		if not u.validPassword(pwd):
-			self.log("Password incorrecta")
-			return abort(401, message="Bad login")
-		self.log("%s autenticado" % u)		
-		return {"status": "ok", "token": str(u.id)}
+        u = Usuario.getUsuarioByEmail(email)
+        if u is None:
+            self.log("Email no existe")
+            return abort(401, message="Bad login")
+        if not u.validPassword(pwd):
+            self.log("Password incorrecta")
+            return abort(401, message="Bad login")
+        self.log("%s autenticado" % u)      
+        return {"status": "ok", "token": str(u.id)}
 
 
 class v2Medidas(NewResource):
-	LOG_TAG = "Medidas"
+    LOG_TAG = "Medidas"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('token', help="token missing", required=True, location='headers')
-		args = parser.parse_args()
-		token = args['token']
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('token', help="token missing", required=True, location='headers')
+        args = parser.parse_args()
+        token = args['token']
 
-		self.log("Token %s" % (token))
+        self.log("Token %s" % (token))
 
-		u = decodeToken(token)
-		if u is None:
-			self.log("Usuario no encontrado")
-			return abort(401, message="Bad token")
-		self.log("%s autorizado" % u)
-		return {"medidas": {
-					'right_arm': u.right_arm,
-					'left_arm': u.left_arm,
-					'right_leg': u.right_leg,
-					'left_leg': u.left_leg,
-					'waist': u.waist,
-					'hip': u.hip,
-					'chest': u.chest,
-					'bust': u.bust,}
-		}
+        u = decodeToken(token)
+        if u is None:
+            self.log("Usuario no encontrado")
+            return abort(401, message="Bad token")
+        self.log("%s autorizado" % u)
+        return {"medidas": {
+                    'right_arm': u.right_arm,
+                    'left_arm': u.left_arm,
+                    'right_leg': u.right_leg,
+                    'left_leg': u.left_leg,
+                    'waist': u.waist,
+                    'hip': u.hip,
+                    'chest': u.chest,
+                    'bust': u.bust,}
+        }
 
 class v2Tallas(NewResource):
-	LOG_TAG = "Tallas"
+    LOG_TAG = "Tallas"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('token', help="token missing", required=True, location='headers')
-		args = parser.parse_args()
-		token = args['token']
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('token', help="token missing", required=True, location='headers')
+        args = parser.parse_args()
+        token = args['token']
 
-		self.log("Token %s" % (token))
+        self.log("Token %s" % (token))
 
-		u = decodeToken(token)
-		if u is None:
-			self.log("Usuario no encontrado")
-			return abort(401, message="Bad token")
-		self.log("%s autorizado" % u)
-		return getTallas(u)
+        u = decodeToken(token)
+        if u is None:
+            self.log("Usuario no encontrado")
+            return abort(401, message="Bad token")
+        self.log("%s autorizado" % u)
+        return getTallas(u)
 
 class v2Perfil(NewResource):
-	LOG_TAG = "Perfil"
+    LOG_TAG = "Perfil"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('token', help="token missing", required=True, location='headers')
-		args = parser.parse_args()
-		token = args['token']
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('token', help="token missing", required=True, location='headers')
+        args = parser.parse_args()
+        token = args['token']
 
-		self.log("Token %s" % (token))
+        self.log("Token %s" % (token))
 
-		u = decodeToken(token)
-		if u is None:
-			self.log("Usuario no encontrado")
-			return abort(401, message="Bad token")
-		self.log("%s autorizado" % u)
-		return {"perfil": {
-					'nombre': u.nombre,
-					'rut': u.rut,
-					'email': u.email,
-					'genero': u.gender}
-		}
+        u = decodeToken(token)
+        if u is None:
+            self.log("Usuario no encontrado")
+            return abort(401, message="Bad token")
+        self.log("%s autorizado" % u)
+        return {"perfil": {
+                      
+                    'nombre': u.nombre,
+                    'rut': u.rut,
+                    'email': u.email,
+                    'genero': u.gender}
+        }
 
 
 class v2Upload(NewResource):
-	"""Docstring"""
-	LOG_TAG = "Upload"
+    """Docstring"""
+    LOG_TAG = "Upload"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('token', required=False, location='headers')
-		from werkzeug.datastructures import FileStorage
-		parser.add_argument("frontalphoto", help="photo missing", type=FileStorage, required=True, location='files')
-		parser.add_argument("lateralphoto", help="photo missing", type=FileStorage, required=True, location='files')
-		parser.add_argument("height", help="height missing", required=True)
-		args = parser.parse_args()
-		token = args['token']
-		frontal_photo = args['frontalphoto']
-		lateral_photo = args['lateralphoto']
-		height = args['height']
+    def post(self):
+        super().post()
 
-		self.log("Frontal photo: %s,Lateral photo: %s,height: %s, token: %s" % (frontal_photo.filename, lateral_photo.filename, height, token))
-		
-		medidas, mensaje = tf.handle_photo(frontal_photo, lateral_photo, height)
-		
-		if medidas is not None and token is not None:
-			u = decodeToken(token)
-			if u is not None:
-				u.guardarMedidas(medidas)
-			#self.log("Usuario no encontrado")
-		
-		return {"response": "upload", "medidas": medidas, "mensaje": mensaje}
+        parser = reqparse.RequestParser()
+        parser.add_argument('token', required=False, location='headers')
+        from werkzeug.datastructures import FileStorage
+        parser.add_argument("frontalphoto", help="photo missing", type=FileStorage, required=True, location='files')
+        parser.add_argument("lateralphoto", help="photo missing", type=FileStorage, required=True, location='files')
+        parser.add_argument("height", help="height missing", required=True)
+        args = parser.parse_args()
+        token = args['token']
+        frontal_photo = args['frontalphoto']
+        lateral_photo = args['lateralphoto']
+        height = args['height']
+
+        self.log("Frontal photo: %s,Lateral photo: %s,height: %s, token: %s" % (frontal_photo.filename, lateral_photo.filename, height, token))
+        
+        medidas, mensaje = tf.handle_photo(frontal_photo, lateral_photo, height)
+        
+        if medidas is not None and token is not None:
+            try:
+                token = int(token)
+                u = decodeToken(token)
+                if u is not None:
+                    u.guardarMedidas(medidas)
+                    self.log("Medidas actualizadas para token %d" % token)
+                else:
+                    self.log("Usuario no encontrado")
+            except:
+                pass
+          
+        
+        
+        return {"response": "upload", "medidas": medidas, "mensaje": mensaje}
 
 
 class v2Register(NewResource):
-	LOG_TAG = "Register"
+    LOG_TAG = "Register"
 
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument('email', help="email missing", required=True)
-		parser.add_argument('pwd', help="pwd missing", required=True)
-		parser.add_argument('nombre')
-		parser.add_argument('rut')
-		parser.add_argument('gender')
-		args = parser.parse_args()
-		email = args['email']
-		pwd = args['pwd']
-		nombre = args['nombre']
-		rut = args['rut']
-		gender = args['gender']
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', help="email missing", required=True)
+        parser.add_argument('pwd', help="pwd missing", required=True)
+        parser.add_argument('nombre')
+        parser.add_argument('rut')
+        parser.add_argument('gender')
+        args = parser.parse_args()
+        email = args['email']
+        pwd = args['pwd']
+        nombre = args['nombre']
+        rut = args['rut']
+        gender = args['gender']
 
-		self.log("%s:%s:%s:%s:%s" % (email, pwd, nombre, rut, gender))
+        self.log("%s:%s:%s:%s:%s" % (email, pwd, nombre, rut, gender))
 
-		u = Usuario.getUsuarioByEmail(email)
-		if u is not None:
-			self.log("Email ya existe")
-			return abort(400, message="Email already exists")
-		
-		u = Usuario.addUsuario(email=email, pwd=pwd, nombre=nombre, rut=rut, gender=gender)
-		token = makeToken(u)
-		self.log("%s registrado" % u)		
-		return {"response": "registrado", "token": token}
+        u = Usuario.getUsuarioByEmail(email)
+        if u is not None:
+            self.log("Email ya existe")
+            return abort(400, message="Email already exists")
+        
+        u = Usuario.addUsuario(email=email, pwd=pwd, nombre=nombre, rut=rut, gender=gender)
+        token = makeToken(u)
+        self.log("%s registrado" % u)       
+        return {"response": "registrado", "token": token}
 
 
 api.add_resource(v2Login, '/login')
@@ -250,37 +264,41 @@ df = pd.read_csv('tallas.csv')
 
 # Tokens extremadamente basicos: solo la id del usuario
 def makeToken(user):
-	return str(user.id)
+    return str(user.id)
 
 
 def decodeToken(token=None):
-	if token is None: return None
+    if token is None: return None
 
-	try:
-		id = int(token)
-		return Usuario.getUsuarioByID(id)
-	except:
-		return None
+    try:
+        id = int(token)
+              
+                    
+        return Usuario.getUsuarioByID(id)
+          
+          
+    except:
+        return None
 
 def getTallas(usuario):
-	print("buscando tallas")
-	right_arm = float(usuario.right_arm)
-	left_arm = float(usuario.left_arm)
-	right_leg = float(usuario.right_leg)
-	left_leg = float(usuario.left_leg)
-	waist = float(usuario.waist)
-	hip = float(usuario.hip)
-	chest = float(usuario.chest)
-	bust = float(usuario.bust)
-	gender = usuario.gender
-	table = df.loc[df['genero'] == gender].values.tolist()
-	print(table)
-	return 0
+    print("buscando tallas")
+    right_arm = float(usuario.right_arm)
+    left_arm = float(usuario.left_arm)
+    right_leg = float(usuario.right_leg)
+    left_leg = float(usuario.left_leg)
+    waist = float(usuario.waist)
+    hip = float(usuario.hip)
+    chest = float(usuario.chest)
+    bust = float(usuario.bust)
+    gender = usuario.gender
+    table = df.loc[df['genero'] == gender].values.tolist()
+    print(table)
+    return 0
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return {"message": "404"}
+    return {"message": "404"}
 
 
 #EOF
--- tf.py
+++ tf.py
@@ -5,78 +5,83 @@ from datetime import datetime as dt
 DBG = 1
 
 class tfManager:
-	PHOTO_FOLDER = '%s%s' % (os.getcwd(), '\\input')
-	TF_ENABLED = 0
+    PHOTO_FOLDER = '%s%s' % (os.getcwd(), '\\input')
+    TF_ENABLED = 0
 
-	def __init__(self, TF_ENABLED=False):
-		self.TF_ENABLED = TF_ENABLED
+    def __init__(self, TF_ENABLED=False):
+        self.TF_ENABLED = TF_ENABLED
 
-		if not os.path.exists(self.PHOTO_FOLDER):
-			os.makedirs(self.PHOTO_FOLDER)
+        if not os.path.exists(self.PHOTO_FOLDER):
+            os.makedirs(self.PHOTO_FOLDER)
 
-		if TF_ENABLED:
-			print(self.PHOTO_FOLDER)
-			# Si se debe iniciar TF, hacemos los imports
-			from scripts.testpersona import MyDetector
-			from scripts.OpenPoseImageV2 import open_pose_image
-			from scripts.get_measures import get_measurements
-			self.open_pose_image = open_pose_image
+        if TF_ENABLED:
+            print(self.PHOTO_FOLDER)
+            # Si se debe iniciar TF, hacemos los imports
+            from scripts.testpersona import MyDetector
+            from scripts.OpenPoseImageV2 import open_pose_image
+            from scripts.get_measures import get_measurements
+            self.open_pose_image = open_pose_image
 
-			# Y cargamos el modelo detector de personas
-			t1 = time.time()
-			self.mDetector = MyDetector()
-			t2 = time.time()
-			if DBG: print('TF iniciado en %.1f [s]' % (t2-t1))
-		else:
-			if DBG: print('TF sin iniciarse')
-			pass
+            # Y cargamos el modelo detector de personas
+            t1 = time.time()
+            self.mDetector = MyDetector()
+            t2 = time.time()
+            if DBG: print('TF iniciado en %.1f [s]' % (t2-t1))
+        else:
+            if DBG: print('TF sin iniciarse')
+            pass
 
 
-	def handle_photo(self, frontal_photo, lateral_photo, height):
-		medidas = None
-		mensaje = None
+    def handle_photo(self, frontal_photo, lateral_photo, height):
+        medidas = None
+        mensaje = None
+        
+        try:
+            height = int(height)
+        except:
+            height = 174                   
+        szNow = dt.now().strftime("%Y-%m-%d_%H.%M.%S")
+        frontal_photo_path = "%s\\frontal_photo_%s.jpg" % (self.PHOTO_FOLDER, szNow)
+        lateral_photo_path = "%s\\lateral_photo_%s.jpg" % (self.PHOTO_FOLDER, szNow)
+        frontal_photo.save(frontal_photo_path)
+        lateral_photo.save(lateral_photo_path)
+        full_frontal_photo_path = os.path.abspath(frontal_photo_path)
+        print(">> Saved: %s" % full_frontal_photo_path)
+        full_lateral_photo_path = os.path.abspath(lateral_photo_path)
+        print(">> Saved: %s" % full_lateral_photo_path)
 
-		szNow = dt.now().strftime("%Y-%m-%d_%H.%M.%S")
-		frontal_photo_path = "%s\\frontal_photo_%s.jpg" % (self.PHOTO_FOLDER, szNow)
-		lateral_photo_path = "%s\\lateral_photo_%s.jpg" % (self.PHOTO_FOLDER, szNow)
-		frontal_photo.save(frontal_photo_path)
-		lateral_photo.save(lateral_photo_path)
-		full_frontal_photo_path = os.path.abspath(frontal_photo_path)
-		print(">> Saved: %s" % full_frontal_photo_path)
-		full_lateral_photo_path = os.path.abspath(lateral_photo_path)
-		print(">> Saved: %s" % full_lateral_photo_path)
+        if not self.TF_ENABLED:
+            return medidas, "TensorFlow desactivado"
 
-		if not self.TF_ENABLED:
-			return medidas, "TensorFlow desactivado"
+        EXTRA_TRIES = 3
+        TRIES = 1 or EXTRA_TRIES
 
-		EXTRA_TRIES = 3
-		TRIES = 1 or EXTRA_TRIES
+        # Si pPersona >= 0.7...
+        # if person_detector(full_frontal_path):
+        bIgnorarHumano = False
+        if bIgnorarHumano or self.mDetector.person_detector(full_frontal_photo_path):
+            if DBG: print('yes_human')
+            # Intentamos calcular las medidas TRIES veces
+            for i in range(TRIES):
+                try:
+                    #medidas = self.open_pose_image(full_frontal_photo_path ,height)
+                    medidas = self.get_measurements(full_frontal_photo_path, full_lateral_photo_path ,height)
+                    if DBG: print(medidas)
 
-		# Si pPersona >= 0.7...
-		# if person_detector(full_frontal_path):
-		if self.mDetector.person_detector(full_frontal_photo_path):
-			if DBG: print('yes_human')
-			# Intentamos calcular las medidas TRIES veces
-			for i in range(TRIES):
-				try:
-					#medidas = self.open_pose_image(full_frontal_photo_path ,height)
-					medidas = self.get_measurements(full_frontal_photo_path, full_lateral_photo_path ,height)
-					if DBG: print(medidas)
+                    mensaje = 'success',
+                    break
+                # Si la medicion falla...
+                except Exception as e:
+                    mensaje = "No pudimos tomar las medidas. ¡Intenta otra vez!"
 
-					mensaje = 'success',
-					break
-				# Si la medicion falla...
-				except Exception as e:
-					mensaje = "No pudimos tomar las medidas. ¡Intenta otra vez!"
-
-					if (DBG):
-						print(e)
-						mensaje = "%s %s" % (mensaje, str(e))
-		# Si pPersona < 0.7
-		else:
-			mensaje = "No pudimos detectar a una persona."
-			if DBG:
-				print('no_human')
-				mensaje = "%s %s" % (mensaje, 'no_human')
-		
-		return medidas, mensaje
+                    if (DBG):
+                        print(e)
+                        mensaje = "%s %s" % (mensaje, str(e))
+        # Si pPersona < 0.7
+        else:
+            mensaje = "No pudimos detectar a una persona."
+            if DBG:
+                print('no_human')
+                mensaje = "%s %s" % (mensaje, 'no_human')
+        
+        return medidas, mensaje
